f868f03ab426cc2434165b05d34b0a487aca00e3
rodrigo-morais <morais.rm@gmail.com>
2017-12-18 10:31:52 +0100

    Filtering by querystring.

--- src/books/controller.js
+++ src/books/controller.js
@@ -1,5 +1,21 @@
 const booksData = require('./data')
 
-const getAll = () => booksData
+const getByGenre = (books, genre) =>
+  books.filter(book => book.genre === genre)
+
+const getByAuthor = (books, author) =>
+  books.filter(book => book.author === author)
+
+const getAll = (genre, author) => {
+  const books =  genre ?
+    getByGenre(booksData, genre) :
+    booksData
+
+  return author ?
+    getByAuthor(books, author) :
+    books
+}
+
+
 
 module.exports = { getAll }
--- src/books/routes.js
+++ src/books/routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const books = require('./controller')
 
 router
-  .get('/', (req, res) => res.send(books.getAll()))
+  .get('/', (req, res) => res.send(books.getAll(req.query.genre, req.query.author)))
 
 
 module.exports = router
